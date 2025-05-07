240e9348df32500408bca6a5dc00dbc5652089d4
KamilMajkut25 <173492@stud.prz.edu.pl>
2025-05-07 12:35:27 +0000

    5_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Collection;
 
 
 
@@ -30,39 +31,54 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    try {
-                        System.out.println("Podaj imię studenta:");
-                        String name = scanner.nextLine();
+                try {
+                    System.out.println("Podaj imię studenta:");
+                    String name = scanner.nextLine();
 
-                        System.out.println("Podaj nazwisko studenta:");
-                        String surname = scanner.nextLine();
+                    System.out.println("Podaj nazwisko studenta:");
+                    String surname = scanner.nextLine();
 
-                        System.out.println("Podaj wiek studenta:");
-                        int age = scanner.nextInt();
-                        scanner.nextLine();
+                    System.out.println("Podaj wiek studenta:");
+                    int age = scanner.nextInt();
+                    scanner.nextLine();
 
-                        System.out.println("Podaj dzień urodzenia (1-31):");
-                        int day = scanner.nextInt();
-                        scanner.nextLine();
+                    System.out.println("Podaj dzień urodzenia (1-31):");
+                    int day = scanner.nextInt();
+                    scanner.nextLine();
 
-                        System.out.println("Podaj miesiąc urodzenia (1-12):");
-                        int month = scanner.nextInt();
-                        scanner.nextLine();
-
-                        System.out.println("Podaj rok urodzenia (1900-2025):");
-                        int year = scanner.nextInt();
-                        scanner.nextLine();
-
-                        String birthDate = String.format("%02d-%02d-%d", day, month, year);
-                        Student newStudent = new Student(name, surname, age, birthDate);
-
-                        s.addStudent(newStudent);
-                        System.out.println("Dodano studenta!");
-                    } catch (Exception e) {
-                        System.out.println("Wystąpił błąd.");
-                        scanner.nextLine(); 
+                    if (day < 1 || day > 31) {
+                        System.out.println("Błędny dzień. Powinien być w zakresie 1-31.");
+                        break;
                     }
-                    break;
+
+                    System.out.println("Podaj miesiąc urodzenia (1-12):");
+                    int month = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (month < 1 || month > 12) {
+                        System.out.println("Błędny miesiąc. Powinien być w zakresie 1-12.");
+                        break;
+                    }
+
+                    System.out.println("Podaj rok urodzenia (1900-2025):");
+                    int year = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (year < 1900 || year > 2025) {
+                        System.out.println("Błędny rok. Powinien być w zakresie 1900-2025.");
+                        break;
+                    }
+
+                    String birthDate = String.format("%02d-%02d-%d", day, month, year);
+                    Student newStudent = new Student(name, surname, age, birthDate);
+
+                    s.addStudent(newStudent);
+                    System.out.println("Dodano studenta!");
+                } catch (Exception e) {
+                    System.out.println("Wystąpił błąd.");
+                    scanner.nextLine();
+                }
+                break;
 
                 case 2:
                     System.out.println("Lista wszystkich studentów:");
